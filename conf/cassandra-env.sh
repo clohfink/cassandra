93fbec9fd560c8f060334a863bc77b7e0fe1cc02
Joseph Lynch <josephl@netflix.com>
2018-10-30 11:44:46 -0700

    CONFIG: Fix the ordering of Priam config in cassandra-env.sh

--- conf/cassandra-env.sh
+++ conf/cassandra-env.sh
@@ -148,7 +148,27 @@ done
 
 JVM_OPTS="$JVM_OPTS -Djava.rmi.server.hostname='$EC2_PUBLIC_HOSTNAME'"
 
-# Check what parameters were defined on jvm.options file to avoid conflicts
+# GC log path has to be defined here because it needs to access CASSANDRA_HOME
+if [ $JAVA_VERSION -ge 11 ] ; then
+    # See description of https://bugs.openjdk.java.net/browse/JDK-8046148 for details about the syntax
+    # The following is the equivalent to -XX:+PrintGCDetails -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=10M
+    echo "$JVM_OPTS" | grep -q "^-[X]log:gc"
+    if [ "$?" = "1" ] ; then # [X] to prevent ccm from replacing this line
+        # only add -Xlog:gc if it's not mentioned in jvm-server.options file
+        mkdir -p ${CASSANDRA_HOME}/logs
+        JVM_OPTS="$JVM_OPTS -Xlog:gc=info,heap*=trace,age*=debug,safepoint=info,promotion*=trace:file=${CASS_LOGS_DIR}/cassandra-${START_TIMESTAMP}-gc.log:time,uptime,pid,tid,level:filecount=10,filesize=10485760"
+    fi
+else
+    # Java 8
+    echo "$JVM_OPTS" | grep -q "^-[X]loggc"
+    if [ "$?" = "1" ] ; then # [X] to prevent ccm from replacing this line
+        # only add -Xlog:gc if it's not mentioned in jvm-server.options file
+        mkdir -p ${CASSANDRA_HOME}/logs
+        JVM_OPTS="$JVM_OPTS -Xloggc:${CASS_LOGS_DIR}/cassandra-${START_TIMESTAMP}-gc.log"
+    fi
+fi
+
+# Check what parameters were defined on jvm-server.options file to avoid conflicts
 echo $JVM_OPTS | grep -q Xmn
 DEFINED_XMN=$?
 echo $JVM_OPTS | grep -q Xmx
