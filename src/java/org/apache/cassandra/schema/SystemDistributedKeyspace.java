3c932af7d2628aba7f0231d71598f34c7b1e5396
ayushis <ayushis@netflix.com>
2022-12-27 13:40:22 -0800

    copy of ca3e1208f85: add QuotasTable to SystemDistributedKeyspace#metadata

--- src/java/org/apache/cassandra/schema/SystemDistributedKeyspace.java
+++ src/java/org/apache/cassandra/schema/SystemDistributedKeyspace.java
@@ -182,7 +182,7 @@ public final class SystemDistributedKeyspace
 
     public static KeyspaceMetadata metadata()
     {
-        return KeyspaceMetadata.create(SchemaConstants.DISTRIBUTED_KEYSPACE_NAME, KeyspaceParams.simple(Math.max(DEFAULT_RF, DatabaseDescriptor.getDefaultKeyspaceRF())), Tables.of(RepairHistory, ParentRepairHistory, ViewBuildStatus, PartitionDenylistTable));
+        return KeyspaceMetadata.create(SchemaConstants.DISTRIBUTED_KEYSPACE_NAME, KeyspaceParams.simple(Math.max(DEFAULT_RF, DatabaseDescriptor.getDefaultKeyspaceRF())), Tables.of(RepairHistory, ParentRepairHistory, ViewBuildStatus, PartitionDenylistTable, QuotasTable));
     }
 
     public static void startParentRepair(TimeUUID parent_id, String keyspaceName, String[] cfnames, RepairOption options)
