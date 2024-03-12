ccef1a7c6bc19ec6330cff13fbe1441c343053a6
ayushis <ayushis@netflix.com>
2024-03-12 09:55:08 -0700

    updated ACCP library

--- src/java/org/apache/cassandra/locator/GossipingPropertyFileSnitch.java
+++ src/java/org/apache/cassandra/locator/GossipingPropertyFileSnitch.java
@@ -25,7 +25,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.cassandra.config.Config;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.db.SystemKeyspace;
 import org.apache.cassandra.exceptions.ConfigurationException;
