d617706ecd0bf5a9fcc768f3f0b5b042dfed6d52
Ammar Khaku <akhaku@netflix.com>
2024-06-25 21:55:02 -0700

    CASS-3271 remove now-unused build_version import
    d41afac8bf9 removed the standalone import in favor of a
    try/except but we somehow restored the standalone import,
    perhaps with a bad merge.
    Remove the import and try/except entirely since we removed
    usage of it with 11e94f1f5a7 anyway.

--- bin/cqlsh.py
+++ bin/cqlsh.py
@@ -155,11 +155,6 @@ from cqlshlib.formatting import (DEFAULT_DATE_FORMAT, DEFAULT_NANOTIME_FORMAT,
 from cqlshlib.tracing import print_trace, print_trace_session
 from cqlshlib.util import get_file_encoding_bomsize
 from cqlshlib.util import is_file_secure
-from cqlshlib.serverversion import version as build_version
-try:
-    from cqlshlib.serverversion import version as build_version
-except ImportError:
-    build_version = "UNKNOWN"
 
 
 DEFAULT_HOST = '127.0.0.1'
