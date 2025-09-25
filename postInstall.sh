db20201e5f48f9f76233b6b379e95ff9e03dc52e
Chris Lohfink <clohfink@netflix.com>
2025-09-25 14:30:47 -0500

    Fix recreate ln2 (#397)

--- postInstall.sh
+++ postInstall.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 ln -s /var/lib/nflx-configs/cassandra/cassandra.yaml /etc/cassandra/ods-cass-conf.yaml || echo "Warning: Failed to create symbolic link /etc/cassandra/ods-cass-conf.yaml"
-# this is needed for priam when it starts up it tries to create backup directories
-mkdir /mnt/data/cassandra || echo "Warning: Failed to create directory /mnt/data/cassandra"
-chmod www-data:www-data /mnt/data/cassandra || echo "Warning: Failed to change ownership of /mnt/data/cassandra"
