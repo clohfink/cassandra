9e1b1fe13879b09c62301f54cbca803d280b1f9f
Chris Lohfink <clohfink@netflix.com>
2025-09-25 11:20:06 -0500

    Add base cassandra directory so priam can startup first time (#395)

--- postInstall.sh
+++ postInstall.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ln -s /var/lib/nflx-configs/cassandra/cassandra.yaml /etc/cassandra/ods-cass-conf.yaml
+# this is needed for priam when it starts up it tries to create backup directories
+mkdir /mnt/data/cassandra
+chmod www-data:www-data /mnt/data/cassandra
